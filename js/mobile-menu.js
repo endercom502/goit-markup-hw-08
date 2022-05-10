b9c8bf4f304b6587a082c234a6ed2e470405e633
Andrzej Markiewicz <endercom502@gmail.com>
2022-05-10 18:44:16 +0200

    10/05-18:44

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector(".start-menu"),
-    closeModalBtn: document.querySelector(".mobile-menu__body"),
-    backdrop: document.querySelector(".end-menu"),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('.start-menu'),
+    closeModalBtn: document.querySelector('.end-menu'),
+    modal: document.querySelector('.mobile-menus'),
+  };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
